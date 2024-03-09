22c55c151dc0dbf6ce83a5f467b5371f3f669993
xerhaxs <xerhaxs@protonmail.com>
2024-03-09 08:28:14 +0100

    fix bugs

--- system-modules/disko-config.nix
+++ system-modules/disko-config.nix
@@ -29,7 +29,7 @@
               size = "100%";
               content = {
                 type = "luks";
-                name = "CRYPTDRIVE";;
+                name = "CRYPTDRIVE";
                 extraOpenArgs = [ "--cipher aes-xts-plain64" "--key-size 512" "--hash sha512" ];
                 settings = {
                   # if you want to use the key for interactive login be sure there is no trailing newline
