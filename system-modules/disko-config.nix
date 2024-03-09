c935c2285513a3988240bb70aa935ac633f6183d
xerhaxs <xerhaxs@protonmail.com>
2024-03-09 09:18:24 +0100

    fix bug

--- system-modules/disko-config.nix
+++ system-modules/disko-config.nix
@@ -31,7 +31,6 @@
               content = {
                 type = "luks";
                 name = "LUKS";
-                hash = "sha512";
                 extraOpenArgs = [ "--cipher aes-xts-plain64" "--key-size 512" "--hash sha512" ];
                 settings = {
                   # if you want to use the key for interactive login be sure there is no trailing newline
