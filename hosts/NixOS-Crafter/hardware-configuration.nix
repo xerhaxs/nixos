bbca4931bb76f4554839807f1613e4eb996fe6d5
xerhaxs <xerhaxs@protonmail.com>
2024-03-09 11:42:45 +0100

    fix bugs

--- hosts/NixOS-Crafter/hardware-configuration.nix
+++ hosts/NixOS-Crafter/hardware-configuration.nix
@@ -12,22 +12,6 @@
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.extraModulePackages = [ ];
 
-
-  fileSystems."/" = {
-    device = "/dev/mapper/crypt-root";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/UEFI";
-    fsType = "vfat";
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/mapper/crypt-home";
-    fsType = "ext4";
-  };
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
