f86fbe256fcb571967c225a8e453bddd7596badb
xerhaxs <xerhaxs@protonmail.com>
2024-03-09 08:02:17 +0100

    Add disko devices to flakes

---
+++ system-modules/disko-config.nix
@@ -0,0 +1,79 @@
+{ disks ? [ "/dev/vda" ], ... }: {
+
+# sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko-config.nix
+# (optional) nixos-generate-config --no-filesystems --root /mnt
+
+{
+  disko.devices = {
+    disk = {
+      vda = {
+        type = "disk";
+        device = builtins.elemAt disks 0;
+        label = "CRYPTDRIVE";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "500M";
+              type = "EF00";
+              name = "BOOT";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypt";
+                extraOpenArgs = [ "--cipher aes-xts-plain64" "--key-size 512" "--hash sha512" ];
+                settings = {
+                  # if you want to use the key for interactive login be sure there is no trailing newline
+                  # for example use `echo -n "password" > /tmp/secret.key`
+                  keyFile = "/tmp/secret.key";
+                  allowDiscards = true;
+                };
+                #additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                content = {
+                  type = "lvm_pv";
+                  vg = "crypt";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      crypt = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "40%FREE";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [
+                "defaults"
+              ];
+            };
+          };
+          home = {
+            size = "100%FREE";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/home";
+            };
+          };
+        };
+      };
+    };
+  };
+}
