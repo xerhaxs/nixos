b3449cb0e839532a597aa7e049f4280fc428a7ba
xerhaxs <xerhaxs@protonmail.com>
2024-03-09 17:00:20 +0100

    found bug?

--- hosts/NixOS-Server/mount.nix
+++ hosts/NixOS-Server/mount.nix
@@ -1,20 +1,20 @@
 { config, pkgs, ... }:
 
 {
-  environment.etc.crypttab = {
-    enable = true;
-    text = ''
-      var       /dev/disk/by-partlabel/disk-VAR-luks 	    /root/keyfile.key 	luks
-    '';
-  };
+  #environment.etc.crypttab = {
+  #  enable = true;
+  #  text = ''
+  #    var       /dev/disk/by-partlabel/disk-VAR-luks 	    /root/keyfile.key 	luks
+  #  '';
+  #};
 
   fileSystems."/var" = {
-    device = "/dev/crypt/var";
+    device = "/dev/crypt2/var";
     fsType = "ext4";
-    #encrypted = {
-    #  enable = true;
-    #  blkDev = "/dev/disk/by-partlabel/disk-VAR-luks";
-    #  keyFile = "/root/keyfile.key";
-    #};
+    encrypted = {
+      enable = true;
+      blkDev = "/dev/disk/by-partlabel/disk-VAR-luks";
+      keyFile = "/root/keyfile.key";
+    };
   };
 }
