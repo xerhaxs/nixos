259d84513accc49acf909820a0bd4b59b560d4ab
xerhaxs <xerhaxs@protonmail.com>
2024-03-10 14:36:51 +0100

    remove module

--- hosts/NixOS-Laptop/hardware-configuration.nix
+++ hosts/NixOS-Laptop/hardware-configuration.nix
@@ -8,7 +8,7 @@
      (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "acpi" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
