f4db70a4a8e171dc57e8c4da6ab3dc5aebb2fe2e
xerhaxs <xerhaxs@protonmail.com>
2024-03-10 15:37:10 +0100

    update flake.lock + resort files

--- hosts/NixOS-Laptop/hardware-configuration.nix
+++ hosts/NixOS-Laptop/hardware-configuration.nix
@@ -20,5 +20,4 @@
   # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
--- system-modules/amdcpu.nix
+++ system-modules/amdcpu.nix
@@ -1,8 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  #boot.kernelParams = [ "amd_iommu=on" "iommu=pt" ];
+  boot.kernelParams = [ "amd_iommu=on" "iommu=pt" ];
   boot.kernelModules = [ "kvm-amd" ];
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   
   #hardware = {
   #  enableAllFirmware = true;
