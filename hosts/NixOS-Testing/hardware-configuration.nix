b2d5a0b1c8e871fda8babbd006c5f57f712a671e
xerhaxs <xerhaxs@protonmail.com>
2024-03-11 15:42:55 +0100

    remove usless stuff

--- hosts/NixOS-Testing/hardware-configuration.nix
+++ hosts/NixOS-Testing/hardware-configuration.nix
@@ -18,8 +18,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
-
-  networking.useDHCP = lib.mkDefault true;
+  
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
