f406854b4861080a81d6263f8f78efe250f8f0b6
xerhaxs <xerhaxs@protonmail.com>
2024-03-11 17:04:19 +0100

    fix some bugs + start testing config

--- hosts/NixOS-Testing/default.nix
+++ hosts/NixOS-Testing/default.nix
@@ -9,5 +9,25 @@
     #./powersave.nix
     ./sddm.nix
     ./user.nix
+
+    ../../system/audio.nix
+    ../../system/bluetooth.nix
+    #./bootloader.nix
+    #./clamav.nix
+    ../../system/common.nix
+    ../../system/cron.nix
+    ../../system/dbus.nix
+    ../../system/dconf.nix
+    ../../system/firewall.nix
+    ../../system/input.nix
+    ../../system/locals.nix
+    ../../system/networking.nix
+    #./nixosvm.nix
+    #./samba-client.nix
+    ../../system/sops.nix
+    ../../system/swap.nix
+    #./upower.nix
+    ../../system/xdg.nix
+    ../../system/xserver.nix
   ];
 }
