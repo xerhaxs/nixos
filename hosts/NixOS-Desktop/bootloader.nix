8703f7412a2572eacb64d74fb2ac0b65230ff513
xerhaxs <xerhaxs@protonmail.com>
2024-03-11 16:55:36 +0100

    fix wrong boot menu

--- hosts/NixOS-Desktop/bootloader.nix
+++ hosts/NixOS-Desktop/bootloader.nix
@@ -4,7 +4,7 @@
   boot.loader.grub.gfxmodeEfi = pkgs.lib.mkForce "3840x1600x32";
   boot.initrd.luks.devices = lib.mkDefault {
     "system" = {
-      device = "/dev/disk/by-label/CRYPTDRIVE";
+      device = lib.mkDefault "/dev/disk/by-label/CRYPTDRIVE";
       preLVM = true;
     };
   };
