d0e5405529d9f499f479a345f9748fd90f2cba54
xerhaxs <xerhaxs@protonmail.com>
2024-03-11 15:15:01 +0000

    fix bootloader

--- hosts/NixOS-Desktop/bootloader.nix
+++ hosts/NixOS-Desktop/bootloader.nix
@@ -2,4 +2,10 @@
 
 {
   boot.loader.grub.gfxmodeEfi = pkgs.lib.mkForce "3840x1600x32";
+  boot.initrd.luks.devices = lib.mkDefault {
+    "system" = {
+      device = "/dev/disk/by-label/CRYPTDRIVE";
+      preLVM = true;
+    };
+  };
 }
