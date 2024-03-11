696a8da738e46cc3540e2ea72ea605667629af7e
xerhaxs <xerhaxs@protonmail.com>
2024-03-11 17:07:59 +0100

    bugfixing + update amd microcode

--- system-modules/amdcpu.nix
+++ system-modules/amdcpu.nix
@@ -3,13 +3,13 @@
 {
   boot.kernelParams = [ "amd_iommu=on" "iommu=pt" ];
   boot.kernelModules = [ "kvm-amd" ];
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  #hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   
-  #hardware = {
-  #  enableAllFirmware = true;
-  #  enableRedistributableFirmware = true;
-  #  cpu.amd.updateMicrocode = true;
-  #};
+  hardware = {
+    enableAllFirmware = true;
+    enableRedistributableFirmware = true;
+    cpu.amd.updateMicrocode = true;
+  };
 
   environment.systemPackages = [
     pkgs.microcodeAmd
