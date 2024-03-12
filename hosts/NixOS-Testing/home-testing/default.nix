fff20eea67ac217e9e70d2759b377b0263aa381b
xerhaxs <xerhaxs@protonmail.com>
2024-03-12 15:35:02 +0100

    test

--- hosts/NixOS-Testing/home-testing/default.nix
+++ hosts/NixOS-Testing/home-testing/default.nix
@@ -4,26 +4,26 @@
   imports = [
     ../../../home
 
-    #../../../home-modules/3dprinting.nix
-    #../../../home-modules/applications.nix
-    #../../../home-modules/editing.nix
+    ../../../home-modules/3dprinting.nix
+    ../../../home-modules/applications.nix
+    ../../../home-modules/editing.nix
     #../../../home-modules/gnome
-    #../../../home-modules/hyprland
+    ../../../home-modules/hyprland
     #../../../home-modules/firefox.nix
-    #../../../home-modules/flameshot.nix
-    #../../../home-modules/hacking.nix
-    #../../../home-modules/kdeconnect.nix
-    #../../../home-modules/librewolf.nix
-    #../../../home-modules/multimedia.nix
-    #../../../home-modules/obs-studio.nix
-    #../../../home-modules/office.nix
-    #../../../home-modules/privacy.nix
-    #../../../home-modules/programming.nix
-    #../../../home-modules/plasma.nix
+    ../../../home-modules/flameshot.nix
+    ../../../home-modules/hacking.nix
+    ../../../home-modules/kdeconnect.nix
+    ../../../home-modules/librewolf.nix
+    ../../../home-modules/multimedia.nix
+    ../../../home-modules/obs-studio.nix
+    ../../../home-modules/office.nix
+    ../../../home-modules/privacy.nix
+    ../../../home-modules/programming.nix
+    ../../../home-modules/plasma.nix
     #../../../home-modules/syncthing.nix
-    #../../../home-modules/theme-latte.nix
+    ../../../home-modules/theme-latte.nix
     #../../../home-modules/thunderbird.nix
-    #../../../home-modules/vscodium.nix
+    ../../../home-modules/vscodium.nix
 
     ./hyprland.nix
     ./xdg.nix
