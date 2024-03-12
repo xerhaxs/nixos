6c20d48d4664779a1550f5600b36837e173ec6c1
xerhaxs <xerhaxs@protonmail.com>
2024-03-12 18:24:40 +0100

    does this fix the temp spikes?

--- hosts/NixOS-Laptop/hardware-configuration.nix
+++ hosts/NixOS-Laptop/hardware-configuration.nix
@@ -10,7 +10,7 @@
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
