01d0e876a7472677179cea7bbc99866079b56dd3
xerhaxs <xerhaxs@protonmail.com>
2024-03-12 15:28:16 +0100

    test

--- flake.nix
+++ flake.nix
@@ -325,8 +325,8 @@
             #./system-modules/amdgpu.nix
             #./system-modules/autostart.nix
             #./system-modules/corectrl.nix
-            ./system-modules/firefox.nix
-            ./system-modules/flatpak.nix
+            #./system-modules/firefox.nix
+            #./system-modules/flatpak.nix
             #./system-modules/fonts.nix
             #./system-modules/gdm.nix
             #./system-modules/gnome.nix
@@ -340,8 +340,8 @@
             #./system-modules/protonmail-bridge.nix
             #./system-modules/python.nix
             ./system-modules/sddm.nix
-            ./system-modules/theme.nix
-            ./system-modules/theme-latte.nix
+            #./system-modules/theme.nix
+            #./system-modules/theme-latte.nix
             #./system-modules/wireguard-client.nix
 
             # disko Moule
