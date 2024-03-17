8d84123a9348959b7dbc9980ed207b90ff45b56c
xerhaxs <xerhaxs@protonmail.com>
2024-03-17 12:03:49 +0100

    test config from turtton for plasma5

--- system-modules/wallpaper-engine-kde-plugin.nix
+++ system-modules/wallpaper-engine-kde-plugin.nix
@@ -1,60 +1,67 @@
-{ mkDerivation
-, fetchFromGitHub
-, cmake
-, extra-cmake-modules
-, plasma-framework
-, gst-libav
-, mpv
-, websockets
-, qtwebsockets
-, qtwebchannel
-, qtdeclarative
-, qtx11extras
-, vulkan-headers
-, vulkan-loader
-, vulkan-tools
-, pkg-config
-, lz4
-, glslang
-}:
-
-let
-  glslang-submodule = mkDerivation {
-    name = "glslang";
-    installPhase = ''
-      mkdir -p $out
-    '';
-    src = fetchFromGitHub {
-      owner = "KhronosGroup";
-      repo = "glslang";
-      rev = "c34bb3b6c55f6ab084124ad964be95a699700d34";
-      sha256 = "IMROcny+b5CpmzEfvKBYDB0QYYvqC5bq3n1S4EQ6sXc=";
-    };
+{username, pkgs, ...} :let 
+# References: https://github.com/brianIcke/nixos-conf/blob/226c97d1b78a527eb0126a7012e27d935d4b4da0/system/BrianTUX/pkgs/wallpaper-engine-plasma-plugin.nix#L37
+glslang-submodule = with pkgs; stdenv.mkDerivation {
+  name = "glslang";
+  installPhase = ''
+    mkdir -p $out
+  '';
+  src = fetchFromGitHub {
+    owner = "KhronosGroup";
+    repo = "glslang";
+    rev = "c34bb3b6c55f6ab084124ad964be95a699700d34";
+    sha256 = "IMROcny+b5CpmzEfvKBYDB0QYYvqC5bq3n1S4EQ6sXc=";
   };
-in
-
-mkDerivation rec {
-  pname = "wallpaper-engine-kde-plugin";
-  version = "0.5.4";
-
-  cmakeFlags = [ "-DUSE_PLASMAPKG=OFF" "-DCMAKE_C_COMPILER=gcc" "-DCMAKE_CXX_COMPILER=g++" ];
-  nativeBuildInputs = [ cmake extra-cmake-modules pkg-config glslang-submodule ];
-  buildInputs = [ 
-    plasma-framework mpv qtwebsockets websockets qtwebchannel
-    qtdeclarative qtx11extras lz4
-    vulkan-headers vulkan-tools vulkan-loader
+};
+wallpaper-engine-kde-plugin = with pkgs; stdenv.mkDerivation rec {
+  pname = "wallpaperEngineKde";
+  version = "91d8e25c0c94b4919f3d110c1f22727932240b3c";
+  src = fetchFromGitHub {
+    owner = "Jelgnum";
+    repo = "wallpaper-engine-kde-plugin";
+    rev = version;
+    hash = "sha256-ff3U/TXr9umQeVHiqfEy38Wau5rJuMeJ3G/CZ9VE++g=";
+    fetchSubmodules = true;
+  };
+  nativeBuildInputs = [
+    cmake extra-cmake-modules glslang-submodule pkg-config gst_all_1.gst-libav shaderc
   ];
-
+  buildInputs = [
+    mpv lz4 vulkan-headers vulkan-tools vulkan-loader
+  ] 
+  ++ (with libsForQt5; with qt5; [plasma-framework qtwebsockets qtwebchannel qtx11extras qtdeclarative])
+  ++ [(python3.withPackages (python-pkgs: [ python-pkgs.websockets ]))];
+  cmakeFlags = [ "-DUSE_PLASMAPKG=OFF" "-DCMAKE_C_COMPILER=gcc" "-DCMAKE_CXX_COMPILER=g++" ];
+  dontWrapQtApps = true;
   postPatch = ''
     rm -rf src/backend_scene/third_party/glslang
     ln -s ${glslang-submodule.src} src/backend_scene/third_party/glslang
   '';
-
-  src = fetchFromGitHub {
-    owner = "catsout";
-    repo = pname;
-    rev = "f972b2a24c9c3cc2d3e4f41d2ebd14f1473cebdf";
-    fetchSubmodules = true;
-    sha256 = "BVtTnJA1RLUU/Tj7WI/80ja4pI8NezHCjKvB72VjrZk=";
+  #Optional informations
+  meta = with lib; {
+    description = "Wallpaper Engine KDE plasma plugin";
+    homepage = "https://github.com/Jelgnum/wallpaper-engine-kde-plugin";
+    license = licenses.gpl2Plus;
+    platforms = platforms.linux;
   };
+};
+in {
+  # Enable the X11 windowing system.
+  services.xserver = {
+    # Enable the KDE Plasma Desktop Environment.
+    displayManager.sddm.enable = true;
+    
+    desktopManager.plasma5.enable = true;
+  };
+  environment.systemPackages = with pkgs; with libsForQt5; [
+    #latte-dock
+    #kdePackages.discover
+    #kdePackages.kgpg
+    #applet-window-buttons
+    #yakuake
+    ### wallpaper-engine-plugin
+    wallpaper-engine-kde-plugin
+    qt5.qtwebsockets
+    (python3.withPackages (python-pkgs: [ python-pkgs.websockets ]))
+    ### 
+  ];
 }
