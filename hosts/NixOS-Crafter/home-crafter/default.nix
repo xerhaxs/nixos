19a72d2e568b3c50118a41818eb2d5e660fdb838
xerhaxs <xerhaxs@protonmail.com>
2024-03-30 18:21:55 +0100

    disable unused thunderbird

--- hosts/NixOS-Crafter/home-crafter/default.nix
+++ hosts/NixOS-Crafter/home-crafter/default.nix
@@ -15,7 +15,7 @@
     ../../../home-modules/plasma5.nix
     #../../../home-modules/syncthing.nix
     ../../../home-modules/theme-latte.nix
-    ../../../home-modules/thunderbird.nix
+    #../../../home-modules/thunderbird.nix
     ../../../home-modules/vscodium.nix
 
     ./xdg.nix
