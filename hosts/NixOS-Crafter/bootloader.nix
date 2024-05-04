a72e0c7323b9d1644a12a58ea2c8c8886ee4c8bd
xerhaxs <xerhaxs@protonmail.com>
2024-05-04 18:42:56 +0200

    fix a bug

--- hosts/NixOS-Crafter/bootloader.nix
+++ hosts/NixOS-Crafter/bootloader.nix
@@ -6,6 +6,7 @@
   boot.loader.grub = {
     device = lib.mkForce "/dev/disk/by-partlabel/disk-NIXOS-BOOT";
     efiSupport = lib.mkForce false;
+    efiInstallAsRemovable = lib.mkForce false;
     gfxmodeBios = "1600x900";
     gfxpayloadBios = "text";
   };
