04f5fd9f3a72f4fe5b2235e269ed7541c61db29c
xerhaxs <xerhaxs@protonmail.com>
2024-05-21 13:58:03 +0200

    add console

---
+++ nixosModules/base/shell/console.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+
+{
+  options.nixos = {
+    base.shell.console = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        example = true;
+        description = "Enable console.";
+      };
+    };
+  };
+
+  config = lib.mkIf config.nixos.base.shell.console.enable {
+    console = {
+      enable = true
+    };
+  };
+}
