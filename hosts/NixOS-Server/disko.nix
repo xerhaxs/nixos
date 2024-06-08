824038a6c775216b7fe7bb3e135c9c999500e5ff
xerhaxs <xerhaxs@protonmail.com>
2024-06-08 23:37:25 +0200

    disable luks for server

--- hosts/NixOS-Server/disko.nix
+++ hosts/NixOS-Server/disko.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }: 
 
 { 
-  nixos.disko.disko-uefi-lvm-on-luks.enable = lib.mkForce true;
+  nixos.disko.disko-uefi-lvm.enable = lib.mkForce true;
   _module.args.disks = [ "/dev/sda" ]; # [ "/dev/sda" "/dev/sdb" ];
 }
