4147020bde57646097554e47a92af45252fd8ad6
xerhaxs <xerhaxs@protonmail.com>
2024-08-15 16:42:30 +0200

    enable dhcp

--- hosts/NixOS-Server/networking.nix
+++ hosts/NixOS-Server/networking.nix
@@ -5,15 +5,15 @@
 
     hostName = "NixOS-Server";
 
-    interfaces = {  
-      enp5s0 = {
-        ipv4.addresses = [ {
-          address = "10.75.0.11";
-          prefixLength = 24;
-        } ];
-      };
-    };
+    #interfaces = {  
+    #  enp5s0 = {
+    #    ipv4.addresses = [ {
+    #      address = "10.75.0.20";
+    #      prefixLength = 24;
+    #    } ];
+    #  };
+    #};
 
-    useDHCP = lib.mkForce false;
+    useDHCP = lib.mkForce true;
   };
 }
