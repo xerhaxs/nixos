689a69963c6b2527eb3df109088964f4cadd4eb8
xerhaxs <xerhaxs@protonmail.com>
2024-08-19 15:48:35 +0200

    add mullvad for server3

--- hosts/NixOS-Server3/server.nix
+++ hosts/NixOS-Server3/server.nix
@@ -6,4 +6,10 @@
   nixos.server.usenet.enable = true;
 
   networking.firewall.enable = lib.mkForce false;
+
+  services.mullvad-vpn.enable = true;
+
+  environment.systemPackages = [
+    pkgs.mullvad
+  ];
 }
