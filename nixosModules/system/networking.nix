384d23d700e7ccac25c2d53c083cc7926ff4ada5
xerhaxs <xerhaxs@protonmail.com>
2024-10-08 19:22:15 +0200

    update hash

--- nixosModules/system/networking.nix
+++ nixosModules/system/networking.nix
@@ -3,7 +3,7 @@
 let
   StevenBlack = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
-    hash = "sha256-oGSXwH6TdMyCCAEuLXmG7O+YZQQ9kvVYuaSNYq0phKM="; #sha256 lib.fakeSha256;
+    hash = "sha256-DuNocnQ4ZuDkAssk4aOqwyp2SM9KFEwAAZkaHed+slA="; #sha256 lib.fakeSha256;
   };
 
   malware = pkgs.fetchurl {
