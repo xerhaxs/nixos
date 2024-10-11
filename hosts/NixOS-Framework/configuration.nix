b530a613562e06ee7f97d4bf4362e65a3f165a5d
xerhaxs <xerhaxs@protonmail.com>
2024-10-11 16:01:14 +0200

    set framework to dark theme

--- hosts/NixOS-Framework/configuration.nix
+++ hosts/NixOS-Framework/configuration.nix
@@ -14,10 +14,10 @@
     };
   };
 
-  nixos.theme.catppuccin = {
-    accent = "Blue";
-    flavor = "Latte";
-  };
+  #nixos.theme.catppuccin = {
+  #  accent = "Blue";
+  #  flavor = "Latte";
+  #};
 
   nixos.hardware = {
     amdcpu.enable = true;
