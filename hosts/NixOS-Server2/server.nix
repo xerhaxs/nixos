0e88d4470e1434cfb0f74809cd2baed92f3f8700
xerhaxs <xerhaxs@protonmail.com>
2024-10-12 19:32:05 +0200

    fix missing "

--- hosts/NixOS-Server2/server.nix
+++ hosts/NixOS-Server2/server.nix
@@ -10,10 +10,10 @@
   nixos.server.game.enable = true;
 
   services.ddclient.domains = [
-    flolserver.${config.nixos.server.network.nginx.domain}
-    creativeserver.${config.nixos.server.network.nginx.domain}
-    pvpserver.${config.nixos.server.network.nginx.domain}
-    testserver.${config.nixos.server.network.nginx.domain}
-    silverageserver.${config.nixos.server.network.nginx.domain}
+    "flolserver.${config.nixos.server.network.nginx.domain}"
+    "creativeserver.${config.nixos.server.network.nginx.domain}"
+    "pvpserver.${config.nixos.server.network.nginx.domain}"
+    "testserver.${config.nixos.server.network.nginx.domain}"
+    "silverageserver.${config.nixos.server.network.nginx.domain}"
   ];
 }
