2e65e911e9d0d9ce7ccb766167450a011bf66f6f
xerhaxs <xerhaxs@protonmail.com>
2024-10-13 14:21:47 +0200

    test

--- nixosModules/disko/disko-uefi-lvm.nix
+++ nixosModules/disko/disko-uefi-lvm.nix
@@ -33,7 +33,7 @@
                   mountOptions = [
                     "defaults"
                   ];
-                  extraArgs = [ '-n BOOT' ];
+                  extraArgs = [ -n BOOT ];
                 };
               };
               primary = {
