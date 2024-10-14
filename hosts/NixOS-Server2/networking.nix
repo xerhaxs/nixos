12987cabb6c825884ba0773836ce8befe8eb3c93
xerhaxs <xerhaxs@protonmail.com>
2024-10-14 18:26:40 +0200

    test

--- hosts/NixOS-Server2/networking.nix
+++ hosts/NixOS-Server2/networking.nix
@@ -5,23 +5,24 @@
 
     hostName = "NixOS-Server2";
 
-    #interfaces = {  
-    #  enp0s18 = {
-    #    ipv4.addresses = [ {
-    #      address = "10.75.0.22";
-    #      prefixLength = 24;
-    #    } ];
-    #    ipv6.address = [ {
-#
-    #    } ];
-    #  };
-    #};
+    interfaces = {  
+      enp0s18 = {
+        ipv4.addresses = [ {
+          address = "10.75.0.22";
+          prefixLength = 24;
+        } ];
+        ipv6.addresses = [ {
+          address = "2001:16b8:a436:8300:be24:11ff:fe84:5084";
+          prefixLength = 64;
+        } ];
+      };
+    };
 
-    #nat.enable = true;
+    nat.enable = true;
 
-    #defaultGateway6 = "fe80::1";
+    defaultGateway6 = "fe80::1";
 
-    useDHCP = lib.mkForce true;
+    useDHCP = lib.mkForce false;
 
     hosts = {
       "127.0.0.1" = lib.mkDefault [
