dacfbdabc6ed03cd6d32e00744d8454934257ca4
xerhaxs <xerhaxs@protonmail.com>
2024-12-18 14:11:01 +0100

    fix convertible

--- hosts/NixOS-Convertible/home/home.nix
+++ hosts/NixOS-Convertible/home/home.nix
@@ -1,7 +1,5 @@
 { config, lib, pkgs, ... }:
 
 {
-  homeManager.applications.enable = lib.mkForce true;
-  homeManager.desktop.desktopEnvironment.enable = lib.mkForce false;
-  homeManager.desktop.windowManager.enable = lib.mkForce false;
+  homeManager.applications.enable = lib.mkForce false;
 }
