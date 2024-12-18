daa7bc5948064472dffae6159839b34ccade0c7b
xerhaxs <xerhaxs@protonmail.com>
2024-12-18 14:37:20 +0100

    test

--- nixosModules/pkgs/linux-wallpaperengine.nix
+++ nixosModules/pkgs/linux-wallpaperengine.nix
@@ -4,6 +4,7 @@
   environment.systemPackages = [
     (pkgs.linux-wallpaperengine.overrideAttrs (old: {
       installPhase = ''
+        cp -r ${/mount/Games/Spiele/Steam/SteamLibrary/steamapps/common/wallpaper_engine/assets}/* $out/assets
         ${lib.optionalString (old ? installPhase) old.installPhase}
         mkdir -p $out/assets
         cp -r /mount/Games/Spiele/Steam/SteamLibrary/steamapps/common/wallpaper_engine/assets/* $out/assets/
