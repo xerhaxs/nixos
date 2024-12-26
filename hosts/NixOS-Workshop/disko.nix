cf47e765eb6c29ab1a477e544dde144fde60937d
xerhaxs <xerhaxs@protonmail.com>
2024-12-26 11:30:45 +0100

    force disko settings for workshop

--- hosts/NixOS-Workshop/disko.nix
+++ hosts/NixOS-Workshop/disko.nix
@@ -3,4 +3,19 @@
 { 
   nixos.disko.disko-uefi-lvm-on-luks.enable = lib.mkForce true;
   _module.args.disks = [ "/dev/sda" ];
+
+  disko.devices = {
+    lvm_vg = {
+      crypt = {
+        lvs = {
+          swap = {
+            size = lib.mkForce "4G";
+          };
+          root = {
+            size = lib.mkForce "60%FREE";
+          };
+        };
+      };
+    };
+  };
 }
