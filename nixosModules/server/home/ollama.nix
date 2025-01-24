a32a77dcd0911ad18bb4ed9421833e7df742121c
xerhaxs <xerhaxs@protonmail.com>
2025-01-24 21:49:03 +0100

    setup ollama

--- nixosModules/server/home/ollama.nix
+++ nixosModules/server/home/ollama.nix
@@ -19,6 +19,13 @@
       port = 11434;
       openFirewall = false;
       models = "\${config.services.ollama.home}/models";
+      loadModels = [
+        deepseek-r1:1.5b
+        deepseek-r1:7b
+        deepseek-r1:8b
+        deepseek-r1:14b
+        deepseek-r1:32b
+      ];
     };
 
     services.nextjs-ollama-llm-ui = {
