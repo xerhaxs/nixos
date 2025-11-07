dbaeb50e8096ee9cf11c61e775f40f7f4be17feb
xerhaxs <github.wjtpq@slmail.me>
2025-11-07 18:38:47 +0100

    final glance colors

--- nixosModules/server/home/glance.nix
+++ nixosModules/server/home/glance.nix
@@ -26,7 +26,7 @@
           # catppuccin-mocha
           background-color = "240 21 15";
           contrast-multiplier = 1.2;
-          primary-color = "217 92 83";
+          primary-color = "267 84 81";
           positive-color = "115 54 76";
           negative-color = "347 70 65";
 
@@ -35,14 +35,14 @@
             catppuccin-frappe = {
               background-color = "229 19 23";
               contrast-multiplier = 1.2;
-              primary-color = "222 74 74";
+              primary-color = "277 59 76";
               positive-color = "96 44 68";
               negative-color = "359 68 71";
             };
             catppuccin-macchiato = {
               background-color = "232 23 18";
               contrast-multiplier = 1.2;
-              primary-color = "220 83 75";
+              primary-color = "267 83 80";
               positive-color = "105 48 72";
               negative-color = "351 74 73";
             };
