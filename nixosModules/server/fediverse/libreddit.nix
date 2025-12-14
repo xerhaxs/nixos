f76ef8aa9990e3a1904188db03287016f384b6e5
xerhaxs <github.wjtpq@slmail.me>
2025-12-14 14:26:23 +0100

    update redlib settings

--- nixosModules/server/fediverse/libreddit.nix
+++ nixosModules/server/fediverse/libreddit.nix
@@ -19,8 +19,27 @@
       port = 8975;
       openFirewall = false;
       settings = {
-        REDLIB_DEFAULT_SHOW_NSFW = "on";
-        
+        SFW_ONLY = "off"; # ["on", "off"]
+        ROBOTS_DISABLE_INDEXING = "on"; # ["on", "off"]
+        ENABLE_RSS = "on"; # ["on", "off"]
+        THEME = "system"; # ["system", "light", "dark", "black", "dracula", "nord", "laserwave", "violet", "gold", "rosebox", "gruvboxdark", "gruvboxlight", "tokyoNight", "icebergDark", "doomone", "libredditBlack", "libredditDark", "libredditLight"]
+        FRONT_PAGE = "default"; # ["default", "popular", "all"]
+        LAYOUT = "card"; # ["card", "clean", "compact"]
+        WIDE = "on"; # ["on", "off"]
+        POST_SORT = "hot"; # ["hot", "new", "top", "rising", "controversial"]
+        COMMENT_SORT = "confidence"; # ["confidence", "top", "new", "controversial", "old"]
+        BLUR_SPOILER = "off"; # ["on", "off"]
+        SHOW_NSFW = "on"; # ["on", "off"]
+        BLUR_NSFW = "off"; # ["on", "off"]
+        USE_HLS = "on"; # ["on", "off"]
+        HIDE_HLS_NOTIFICATION = "off"; # ["on", "off"]
+        AUTOPLAY_VIDEOS = "off"; # ["on", "off"]
+        HIDE_AWARDS = "off"; # ["on", "off"]
+        DISABLE_VISIT_REDDIT_CONFIRMATION = "off"; # ["on", "off"]
+        HIDE_SCORE = "off"; # ["on", "off"]
+        HIDE_SIDEBAR_AND_SUMMARY = "off"; # ["on", "off"]
+        FIXED_NAVBAR = "on"; # ["on", "off"]
+        REMOVE_DEFAULT_FEEDS = "off"; # ["on", "off"]
       };
     };
 
