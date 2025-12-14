a16c7ae11178d55339fb9fd4d3dbe3bbf5a75eb1
xerhaxs <github.wjtpq@slmail.me>
2025-12-14 15:32:54 +0100

    remove lemmy

--- nixosModules/server/fediverse/default.nix
+++ nixosModules/server/fediverse/default.nix
@@ -4,7 +4,6 @@
   imports = [
     ./gitea.nix
     ./invidious.nix
-    ./lemmy.nix
     ./libreddit.nix
     ./moneronode.nix
     ./murmur.nix
@@ -28,7 +27,6 @@
     nixos.server.fediverse = {
       gitea.enable = true;
       invidious.enable = true;
-      lemmy.enable = true;
       libreddit.enable = true;
       moneronode.enable = true;
       murmur.enable = true;
