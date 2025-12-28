238fb46934ec0d29e2064a99adde8fb2f2497d03
xerhaxs <github.wjtpq@slmail.me>
2025-12-28 14:54:10 +0100

    add wallpaper setting to desktop

--- hosts/NixOS-Desktop/home/home.nix
+++ hosts/NixOS-Desktop/home/home.nix
@@ -1,7 +1,17 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, plasma-manager, ... }:
+
+{
+  imports = [
+    plasma-manager.homeModules.plasma-manager
+  ];
 
 {
   homeManager.applications.enable = lib.mkForce true;
 
   #homeManager.desktop.windowManager.hyprland.dunst.enable = lib.mkForce false;
+  programs.plasma = {
+    workspace = {
+      wallpaper = lib.mkForce "${config.xdg.userDirs.pictures}/Desktopbilder/JWST/compress/52338778943_9704c200b4_o.jpg";
+    };
+  };
 }
