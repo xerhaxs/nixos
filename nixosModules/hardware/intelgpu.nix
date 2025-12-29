a7b65aeb15f548dd6f6c1f74d2a6145b63eca33b
xerhaxs <github.wjtpq@slmail.me>
2025-12-29 13:07:54 +0100

    update intel gpu driver

--- nixosModules/hardware/intelgpu.nix
+++ nixosModules/hardware/intelgpu.nix
@@ -21,7 +21,7 @@
       enable = true;
       extraPackages = with pkgs; [
         intel-media-driver
-        vaapiIntel
+        intel-vaapi-driver
         vaapiVdpau
         libvdpau-va-gl
         intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
