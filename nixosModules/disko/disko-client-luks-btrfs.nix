805e367bbd4cd79676e1e169aa7eef13567284e9
xerhaxs <github.wjtpq@slmail.me>
2025-12-31 14:32:16 +0100

    test persistence modules

--- nixosModules/disko/disko-client-luks-btrfs.nix
+++ nixosModules/disko/disko-client-luks-btrfs.nix
@@ -146,6 +146,41 @@
       fileSystems = [ "/" ];
     };
 
+    environment.persistence."/persistent" = {
+      enable = true;
+      hideMounts = false;
+      directories = [
+        #"/var/log"
+        #"/var/lib/bluetooth"
+        #"/var/lib/nixos"
+        #"/var/lib/systemd/coredump"
+        "/root/keys"
+        "/etc/NetworkManager/system-connections"
+        #{ directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
+      ];
+      #files = [
+      #  "/etc/machine-id"
+      #  { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
+      #];
+      users.${config.nixos.system.user.defaultuser.name} = {
+        directories = [
+          "Downloads"
+          "Music"
+          "Pictures"
+          "Documents"
+          "Videos"
+          { directory = ".gnupg"; mode = "0700"; }
+          { directory = ".ssh"; mode = "0700"; }
+          { directory = ".nixops"; mode = "0700"; }
+          { directory = ".local/share/keyrings"; mode = "0700"; }
+          #".local/share/direnv"
+        ];
+        #files = [
+        #  ".screenrc"
+        #];
+      };
+    };
+
     disko.devices = {
       disk = {
         vda = {
