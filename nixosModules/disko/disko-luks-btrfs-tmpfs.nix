2651b39ee2dd1ddf30b20bac5be519b1a8ab9635
xerhaxs <github.wjtpq@slmail.me>
2026-01-07 15:21:29 +0100

    test updated impermanence module

--- nixosModules/disko/disko-luks-btrfs-tmpfs.nix
+++ nixosModules/disko/disko-luks-btrfs-tmpfs.nix
@@ -144,41 +144,41 @@
       files = [
         "/root/.bash_history"
       ];
-      users.${config.nixos.system.user.defaultuser.name} = {
-        directories = [
-          "Desktop"
-          "Documents"
-          "Downloads"
-          "Music"
-          "Pictures"
-          "Videos"
-          { directory = ".gnupg"; mode = "0700"; }
-          { directory = ".ssh"; mode = "0700"; }
-          { directory = ".nixops"; mode = "0700"; }
-          { directory = ".local/share/keyrings"; mode = "0700"; }
-          ".local/share/direnv"
-          ".local/share/baloo"
-          ".local/share/bottles"
-          ".local/share/PrismLauncher" # Create Symlink from Game Drive via HomeManager
-          ".local/share/wasistlos" # WhatsApp
-          ".local/share/TelegramDesktop"
-          ".librewolf"
-          ".mozilla"
-          ".steam"
-          ".thunderbird"
-          ".wine"
-          ".local/cache"
-          ".local/state" # move file settings to nixos
+      #users.${config.nixos.system.user.defaultuser.name} = {
+      #  directories = [
+      #    "Desktop"
+      #    "Documents"
+      #    "Downloads"
+      #    "Music"
+      #    "Pictures"
+      #    "Videos"
+      #    { directory = ".gnupg"; mode = "0700"; }
+      #    { directory = ".ssh"; mode = "0700"; }
+      #    { directory = ".nixops"; mode = "0700"; }
+      #    { directory = ".local/share/keyrings"; mode = "0700"; }
+      #    ".local/share/direnv"
+      #    ".local/share/baloo"
+      #    ".local/share/bottles"
+      #    ".local/share/PrismLauncher" # Create Symlink from Game Drive via HomeManager
+      #    ".local/share/wasistlos" # WhatsApp
+      #    ".local/share/TelegramDesktop"
+      #    ".librewolf"
+      #    ".mozilla"
+      #    ".steam"
+      #    ".thunderbird"
+      #    ".wine"
+      #    ".local/cache"
+      #    ".local/state" # move file settings to nixos
           #{
           #  directory = ".local/share/Steam";
           #  method = "symlink";
           #}
-        ];
-        files = [
-          ".bash_history" # Bash History
-          ".viminfo" # VIM History
-        ];
-      };
+      #  ];
+      #  files = [
+      #    ".bash_history" # Bash History
+      #    ".viminfo" # VIM History
+      #  ];
+      #};
     };
 
     disko.devices = {
