150b4e2d7b366394d4a36c32b452500f534a11db
xerhaxs <github.wjtpq@slmail.me>
2026-01-26 16:22:27 +0100

    set extra config

--- nixosModules/server/fediverse/languagetool.nix
+++ nixosModules/server/fediverse/languagetool.nix
@@ -35,13 +35,13 @@
           http2 = false;
           locations."/" = {
             proxyPass = "http://localhost:8001";
-            proxySetHeader = [
-              "Host $host"
-              "X-Real-IP $remote_addr"
-              "X-Forwarded-For $proxy_add_x_forwarded_for"
-              "X-Forwarded-Proto $scheme"
-              "Content-Type application/x-www-form-urlencoded"
-            ];
+            extraConfig = ''
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+              proxy_set_header Content-Type "application/x-www-form-urlencoded";
+            '';
           };
         };
       };
