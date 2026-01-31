1825ee3bc712905d65af517603580409870313e1
xerhaxs <github.wjtpq@slmail.me>
2026-01-31 12:37:00 +0100

    setup brave

--- homeModules/applications/browser/brave.nix
+++ homeModules/applications/browser/brave.nix
@@ -42,29 +42,29 @@
       ];
     };
 
-    home.file."brave/policies.json".text = ''
-      {
-        "BraveAIChatEnabled": false,
-        "BraveRewardsDisabled": true,
-        "BraveWalletDisabled": true,
-        "BraveVPNDisabled": true,
-        "TorDisabled": true,
-        "BraveP3AEnabled": false,
-        "BraveStatsPingEnabled": false,
-        "BraveWebDiscoveryEnabled": false,
-        "BraveNewsDisabled": true,
-        "BraveTalkDisabled": true,
-        "BraveSpeedreaderEnabled": false,
-        "BraveWaybackMachineEnabled": false,
-        "BravePlaylistEnabled": false,
-        "SyncDisabled": false,
-        "PasswordManagerEnabled": false,
-        "AutofillAddressEnabled": false,
-        "AutofillCreditCardEnabled": false,
-        "TranslateEnabled": false,
-        "DnsOverHttpsMode": "secure",
-        "DnsOverHttpsTemplates": "https://dns.adguard-dns.com/dns-query"
-      }
-    '';
+    #home.file."brave/policies.json".text = ''
+    #  {
+    #    "BraveAIChatEnabled": false,
+    #    "BraveRewardsDisabled": true,
+    #    "BraveWalletDisabled": true,
+    #    "BraveVPNDisabled": true,
+    #    "TorDisabled": true,
+    #    "BraveP3AEnabled": false,
+    #    "BraveStatsPingEnabled": false,
+    #    "BraveWebDiscoveryEnabled": false,
+    #    "BraveNewsDisabled": true,
+    #    "BraveTalkDisabled": true,
+    #    "BraveSpeedreaderEnabled": false,
+    #    "BraveWaybackMachineEnabled": false,
+    #    "BravePlaylistEnabled": false,
+    #    "SyncDisabled": false,
+    #    "PasswordManagerEnabled": false,
+    #    "AutofillAddressEnabled": false,
+    #    "AutofillCreditCardEnabled": false,
+    #    "TranslateEnabled": false,
+    #    "DnsOverHttpsMode": "secure",
+    #    "DnsOverHttpsTemplates": "https://dns.adguard-dns.com/dns-query"
+    #  }
+    #'';
   };
 }
