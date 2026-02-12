4c6ac91904843d707ed91fa80dbd33897017f85c
xerhaxs <github.wjtpq@slmail.me>
2026-02-12 16:01:29 +0100

    revert syncthing

--- homeModules/applications/sync/syncthing.nix
+++ homeModules/applications/sync/syncthing.nix
@@ -16,44 +16,9 @@
     services.syncthing = {
       enable = true;
       tray.enable = false;
-      overrideDevices = true;
-      overrideFolders = true;
-      cert = "pathtocert";
-      key = "pathtokey";
-      guiAddress = "127.0.0.1:8384";
       extraOptions = [
         "--gui-theme=black"
       ];
-
-      settings = {
-        options = {
-          #limitBandwidthInLan
-          localAnnounceEnabled = false;
-          relaysEnabled = false;
-          urAccepted = -1;
-        };
-        devices = {
-          NixOS-Desktop = {
-            autoAcceptFolders = false;
-            name = "‹name›";
-            id = "MFZWI3D-BONSGYC-YLTMRWG-C43ENR5-QXGZDMM-FZWI3DP-BONSGYY-LTMRWAD";
-
-          };
-
-          folders = {
-            "Dokumente" = {
-              enable = true;
-              label = "‹name›";
-              path = "/home/jf/Dokumente";
-              type = "sendreceive"; # "sendreceive", "sendonly", "receiveonly", "receiveencrypted"
-              #versioning.type = "staggered"; # "external", "simple", "staggered", "trashcan"
-              copyOwnershipFromParent = false;
-              id = "FOLDERID";
-              devices = [ "NixOS-Framework" "NixOS-Desktop" ];
-            };
-          };
-        };
-      };
     };
   };
 }
