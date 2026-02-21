e4f088a21f4ae007ed53b254d46df2a1f0ecb440
xerhaxs <github.wjtpq@slmail.me>
2026-02-21 18:14:25 +0100

    enable zfs tools

--- hosts/NixOS-Server1/zfsmount.nix
+++ hosts/NixOS-Server1/zfsmount.nix
@@ -23,6 +23,10 @@ in
   boot.kernelPackages = lib.mkForce latestKernelPackage;
   boot.supportedFilesystems = [ "zfs" ];
 
+  environment.systemPackages = with pkgs; [
+    zfs
+  ];
+
   networking.hostId = "d7b23b42";
 
   services.zfs = {
@@ -30,76 +34,82 @@ in
       enable = true;
       interval = "weekly";
     };
-    /* autoSnapshot = {
-      enable = false;
-      frequent = 4;
-      hourly = 24;
-      daily = 7;
-      weekly = 4;
-      monthly = 12;
-    }; */
+    /*
+      autoSnapshot = {
+        enable = false;
+        frequent = 4;
+        hourly = 24;
+        daily = 7;
+        weekly = 4;
+        monthly = 12;
+      };
+    */
     trim = {
       enable = true;
       interval = "weekly";
     };
   };
 
-  /* fileSystems = {
-    #"/pool01/applications" = {
-    #  device = "pool01/applications";
-    #  fsType = "zfs";
-    #  options = [ "zfsutil" ];
-    #  depends = [ "zfs-load-key.service" ];
-    #};
-    "/pool01/share" = {
-      device = "pool01/share";
-      fsType = "zfs";
-      options = [ "zfsutil" ];
-      depends = [ "zfs-load-key.service" ];
+  /*
+    fileSystems = {
+      #"/pool01/applications" = {
+      #  device = "pool01/applications";
+      #  fsType = "zfs";
+      #  options = [ "zfsutil" ];
+      #  depends = [ "zfs-load-key.service" ];
+      #};
+      "/pool01/share" = {
+        device = "pool01/share";
+        fsType = "zfs";
+        options = [ "zfsutil" ];
+        depends = [ "zfs-load-key.service" ];
+      };
+      #"/pool01/share/jf" = {
+      #  device = "pool01/share/jf";
+      #  fsType = "zfs";
+      #  options = [ "zfsutil" ];
+      #  depends = [ "/pool01/share" ];
+      #};
+      "/pool01/share/music" = {
+        device = "pool01/share/music";
+        fsType = "zfs";
+        options = [ "zfsutil" ];
+        depends = [ "/pool01/share" ];
+      };
+      "/pool01/share/photo" = {
+        device = "pool01/share/photo";
+        fsType = "zfs";
+        options = [ "zfsutil" ];
+        depends = [ "/pool01/share" ];
+      };
+      "/pool01/share/video" = {
+        device = "pool01/share/video";
+        fsType = "zfs";
+        options = [ "zfsutil" ];
+        depends = [ "/pool01/share" ];
+      };
     };
-    #"/pool01/share/jf" = {
-    #  device = "pool01/share/jf";
-    #  fsType = "zfs";
-    #  options = [ "zfsutil" ];
-    #  depends = [ "/pool01/share" ];
-    #};
-    "/pool01/share/music" = {
-      device = "pool01/share/music";
-      fsType = "zfs";
-      options = [ "zfsutil" ];
-      depends = [ "/pool01/share" ];
-    };
-    "/pool01/share/photo" = {
-      device = "pool01/share/photo";
-      fsType = "zfs";
-      options = [ "zfsutil" ];
-      depends = [ "/pool01/share" ];
-    };
-    "/pool01/share/video" = {
-      device = "pool01/share/video";
-      fsType = "zfs";
-      options = [ "zfsutil" ];
-      depends = [ "/pool01/share" ];
-    };
-  }; */
+  */
 
-/*   systemd.services."zfs-load-key" = {
-    description = "Load ZFS encryption key for pool01/share";
-    before = [ "pool01-share.mount" ];
-    after = [
-      "zfs-import.target"
-      "sops-nix.service"
-    ];
-    requires = [ "sops-nix.service" ];
-    wantedBy = [ "zfs-mount.target" ];
-    path = [ pkgs.zfs ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
+  /*
+    systemd.services."zfs-load-key" = {
+      description = "Load ZFS encryption key for pool01/share";
+      before = [ "pool01-share.mount" ];
+      after = [
+        "zfs-import.target"
+        "sops-nix.service"
+      ];
+      requires = [ "sops-nix.service" ];
+      wantedBy = [ "zfs-mount.target" ];
+      path = [ pkgs.zfs ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+      };
+      script = ''
+        cat ${config.sops.secrets."zfs/pool01".path} | \
+        zfs load-key pool01/share
+      '';
     };
-    script = ''
-      cat ${config.sops.secrets."zfs/pool01".path} | \
-      zfs load-key pool01/share
-    '';
-  }; */
+  */
 }
