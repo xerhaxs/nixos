ebcd6d6bcec4910ce252dd8063a85ee21ca3a530
xerhaxs <github.wjtpq@slmail.me>
2026-02-23 15:59:51 +0100

    remove extra-substituters from flakes

--- flake.nix
+++ flake.nix
@@ -1,13 +1,6 @@
 {
   description = "A NixOS configuration";
 
-  nixConfig = {
-    extra-substituters = [ "https://nix-community.cachix.org" ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-colors.url = "github:misterio77/nix-colors";
