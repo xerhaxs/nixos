da6f72c116f760428fc286d10213b8993a487df9
xerhaxs <github.wjtpq@slmail.me>
2026-03-01 18:32:38 +0100

    setup driver for network card during boot

--- hosts/NixOS-Server1/configuration.nix
+++ hosts/NixOS-Server1/configuration.nix
@@ -17,15 +17,21 @@
     ssh = {
       enable = true;
       port = 22;
-      authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGyndaaCmTLHdA5+sLBbxgSZNC6KXKtZkKiMK6AAY8Rt jf@NixOS-Desktop" ];
-      hostKeys = [ "/etc/ssh/initrd_host_key" ]; 
+      authorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGyndaaCmTLHdA5+sLBbxgSZNC6KXKtZkKiMK6AAY8Rt jf@NixOS-Desktop"
+      ];
+      hostKeys = [ "/etc/ssh/initrd_host_key" ];
       # generate keys with "ssh-keygen -t ed25519 -N "" -f /etc/ssh/initrd_host_key"
       # ssh root@10.75.0.21
       # enter disk password "systemd-tty-ask-password-agent --query"
     };
   };
 
-  boot.kernelParams = [ "ip=10.75.0.21::10.75.0.1:255.255.255.0::eno1:none" ];
+  boot.initrd.availableKernelModules = [
+    "r8169"
+  ];
+
+  #boot.kernelParams = [ "ip=10.75.0.21::10.75.0.1:255.255.255.0::eno1:none" ];
 
   boot.initrd.secrets = {
     "/etc/ssh/initrd_host_key" = "/etc/ssh/initrd_host_key";
