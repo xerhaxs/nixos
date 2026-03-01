cc1b97d13302ed87a3043881585823d01a83eb65
xerhaxs <github.wjtpq@slmail.me>
2026-03-01 17:44:40 +0100

    change flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
     nix-colors.url = "github:misterio77/nix-colors";
 
     sops-nix = {
