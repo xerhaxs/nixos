54f0347c46882ce6b3baae5474d23b8c356b24bc
xerhaxs <github.wjtpq@slmail.me>
2026-03-01 18:24:34 +0100

    clean jellyfin

--- nixosModules/server/home/jellyfin.nix
+++ nixosModules/server/home/jellyfin.nix
@@ -33,7 +33,6 @@
         enable = true;
         device = "/dev/dri/renderD129";
         type = "qsv"; # "none", "amf", "qsv", "nvenc", "v4l2m2m", "vaapi", "rkmpp"
-        # !!!! vaapi in proxmox vm needed - qsv for intel arc not working in vms !!!!
       };
 
       transcoding = {
