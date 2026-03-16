0cb95aea1afcb5009662a917eb74bd29f2bae90c
xerhaxs <github.wjtpq@slmail.me>
2026-03-16 14:51:38 +0100

    fix

--- nixosModules/server/home/cockpit.nix
+++ nixosModules/server/home/cockpit.nix
@@ -44,14 +44,6 @@
           locations."/" = {
             proxyPass = "http://127.0.0.1:9090/";
             proxyWebsockets = true;
-            extraConfig = ''
-              proxy_set_header Host $host;
-              proxy_set_header X-Forwarded-Proto $scheme;
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-              proxy_http_version 1.1;
-              proxy_set_header Upgrade $http_upgrade;
-              proxy_set_header Connection "upgrade";
-            '';
           };
         };
       };
