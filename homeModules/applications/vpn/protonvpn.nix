68b52203c1e0cee1a718cad1cc685aabf8a105a9
xerhaxs <github.wjtpq@slmail.me>
2026-03-28 08:50:38 +0100

    update protonvpn

--- homeModules/applications/vpn/protonvpn.nix
+++ homeModules/applications/vpn/protonvpn.nix
@@ -19,7 +19,7 @@
 
   config = lib.mkIf config.homeManager.applications.vpn.protonvpn.enable {
     home.packages = with pkgs; [
-      protonvpn-gui
+      proton-vpn
     ];
   };
 }
--- nixosModules/userEnvironment/mullvad.nix
+++ nixosModules/userEnvironment/mullvad.nix
@@ -26,7 +26,6 @@
 
     environment.systemPackages = with pkgs; [
       mullvad-browser
-      protonvpn-gui
     ];
   };
 }
