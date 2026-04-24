8da12f7ff3a87877b1897e6a4ab608d24e638b86
xerhaxs <github.wjtpq@slmail.me>
2026-04-24 12:14:30 +0200

    add kile config

--- homeModules/applications/office/kile.nix
+++ homeModules/applications/office/kile.nix
@@ -2,6 +2,8 @@
   config,
   lib,
   pkgs,
+  osConfig,
+  
   ...
 }:
 
@@ -28,149 +30,150 @@
     #  enable = true;
     #};
 
-    /* xdg.configFile."kilerc".text = ''
-      [FileBrowserWidget]
-      Allow Expansion=false
-      Decoration position=2
-      Show Inline Previews=true
-      Show hidden files=false
-      Sort by=Name
-      Sort directories first=true
-      Sort hidden files last=false
-      Sort reversed=false
-      View Style=Tree
+    programs.plasma.configFile."kilerc" = {
+      "FileBrowserWidget" = {
+        "Allow Expansion".value = false;
+        "Decoration position".value = 2;
+        "Show Inline Previews".value = true;
+        "Show hidden files".value = false;
+        "Sort by".value = "Name";
+        "Sort directories first".value = true;
+        "Sort hidden files last".value = false;
+        "Sort reversed".value = false;
+        "View Style".value = "Tree";
+      };
 
-      [IncludeGraphics]
-      imagemagick=true
+      "IncludeGraphics" = {
+        "imagemagick".value = true;
+      };
 
-      [KTextEditor Document]
-      Allow End of Line Detection=true
-      Auto Detect Indent=true
-      Auto Reload If Any External Changes Occurs=false
-      Auto Reload If State Is In Version Control=true
-      Auto Save=false
-      Auto Save Interval=0
-      Auto Save On Focus Out=false
-      BOM=false
-      Backup Local=false
-      Backup Prefix=
-      Backup Remote=false
-      Backup Suffix=~
-      Camel Cursor=false
-      Encoding=UTF-8
-      End of Line=0
-      Indent On Backspace=true
-      Indent On Tab=true
-      Indent On Text Paste=true
-      Indentation Mode=normal
-      Indentation Width=4
-      Keep Extra Spaces=false
-      Line Length Limit=10000
-      Newline at End of File=true
-      On-The-Fly Spellcheck=true
-      Overwrite Mode=false
-      PageUp/PageDown Moves Cursor=false
-      Remove Spaces=1
-      ReplaceTabsDyn=true
-      Show Spaces=0
-      Show Tabs=true
-      Smart Home=true
-      Swap Directory=
-      Swap File Mode=1
-      Swap Sync Interval=15
-      Tab Handling=2
-      Tab Width=4
-      Trailing Marker Size=1
-      Use Editor Config=true
-      Word Wrap=false
-      Word Wrap Column=80
+      "KTextEditor Document" = {
+        "Allow End of Line Detection".value = true;
+        "Auto Detect Indent".value = true;
+        "Auto Reload If Any External Changes Occurs".value = false;
+        "Auto Reload If State Is In Version Control".value = true;
+        "Auto Save".value = false;
+        "Auto Save Interval".value = 0;
+        "Auto Save On Focus Out".value = false;
+        "BOM".value = false;
+        "Backup Local".value = false;
+        "Backup Prefix".value = "";
+        "Backup Remote".value = false;
+        "Backup Suffix".value = "~";
+        "Camel Cursor".value = false;
+        "Encoding".value = "UTF-8";
+        "End of Line".value = 0;
+        "Indent On Backspace".value = true;
+        "Indent On Tab".value = true;
+        "Indent On Text Paste".value = true;
+        "Indentation Mode".value = "normal";
+        "Indentation Width".value = 4;
+        "Keep Extra Spaces".value = false;
+        "Line Length Limit".value = 10000;
+        "Newline at End of File".value = true;
+        "On-The-Fly Spellcheck".value = true;
+        "Overwrite Mode".value = false;
+        "PageUp/PageDown Moves Cursor".value = false;
+        "Remove Spaces".value = 1;
+        "ReplaceTabsDyn".value = true;
+        "Show Spaces".value = 0;
+        "Show Tabs".value = true;
+        "Smart Home".value = true;
+        "Swap File Mode".value = 1;
+        "Swap Sync Interval".value = 15;
+        "Tab Handling".value = 2;
+        "Tab Width".value = 4;
+        "Trailing Marker Size".value = 1;
+        "Use Editor Config".value = true;
+        "Word Wrap".value = false;
+        "Word Wrap Column".value = 80;
+      };
 
-      [KTextEditor Editor]
-      Encoding Prober Type=1
-      Fallback Encoding=UTF-16
+      "KTextEditor Editor" = {
+        "Encoding Prober Type".value = 1;
+        "Fallback Encoding".value = "UTF-16";
+      };
 
-      [KTextEditor Renderer]
-      Animate Bracket Matching=false
-      Auto Color Theme Selection=true
-      Color Theme=Catppuccin Mocha
-      Line Height Multiplier=1
-      Show Indentation Lines=false
-      Show Whole Bracket Expression=true
-      Text Font=Hack,10,-1,7,400,0,0,0,0,0,0,0,0,0,0,1
-      Text Font Features=
-      Word Wrap Marker=false
+      "KTextEditor Renderer" = {
+        "Animate Bracket Matching".value = false;
+        "Auto Color Theme Selection".value = true;
+        "Color Theme".value = "Catppuccin ${osConfig.nixos.theme.catppuccin.flavor}";
+        "Line Height Multiplier".value = 1;
+        "Show Indentation Lines".value = false;
+        "Show Whole Bracket Expression".value = true;
+        "Text Font".value = "Hack,10,-1,7,400,0,0,0,0,0,0,0,0,0,0,1";
+        "Text Font Features".value = "";
+        "Word Wrap Marker".value = false;
+      };
 
-      [KTextEditor View]
-      Allow Mark Menu=true
-      Auto Brackets=true
-      Auto Center Lines=0
-      Auto Completion=true
-      Auto Completion Preselect First Entry=true
-      Backspace Remove Composed Characters=false
-      Bookmark Menu Sorting=0
-      Bracket Match Preview=false
-      Chars To Enclose Selection=<>(){}[]'"
-      Cycle Through Bookmarks=true
-      Default Mark Type=1
-      Disable bracket match highlight if inactive=false
-      Disable current line highlight if inactive=false
-      Dynamic Word Wrap=true
-      Dynamic Word Wrap Align Indent=80
-      Dynamic Word Wrap At Static Marker=false
-      Dynamic Word Wrap Indicators=1
-      Dynamic Wrap not at word boundaries=false
-      Enable Accessibility=true
-      Enable Tab completion=false
-      Enter To Insert Completion=true
-      Fold First Line=false
-      Folding Bar=true
-      Folding Preview=true
-      Hide cursor if inactive=false
-      Icon Bar=false
-      Input Mode=0
-      Keyword Completion=true
-      Line Modification=true
-      Line Numbers=true
-      Max Clipboard History Entries=20
-      Maximum Search History Size=100
-      Mouse Paste At Cursor Position=false
-      Multiple Cursor Modifier=134217728
-      Persistent Selection=false
-      Scroll Bar Marks=false
-      Scroll Bar Mini Map All=true
-      Scroll Bar Mini Map Width=60
-      Scroll Bar MiniMap=true
-      Scroll Bar Preview=true
-      Scroll Past End=false
-      Search/Replace Flags=140
-      Shoe Line Ending Type in Statusbar=false
-      Show Documentation With Completion=true
-      Show File Encoding=true
-      Show Folding Icons On Hover Only=true
-      Show Line Count=true
-      Show Scrollbars=0
-      Show Statusbar Dictionary=true
-      Show Statusbar Highlighting Mode=true
-      Show Statusbar Input Mode=true
-      Show Statusbar Line Column=true
-      Show Statusbar Tab Settings=true
-      Show Word Count=true
-      Smart Copy Cut=true
-      Statusbar Line Column Compact Mode=true
-      Text Drag And Drop=true
-      User Sets Of Chars To Enclose Selection=
-      Vi Input Mode Steal Keys=false
-      Vi Relative Line Numbers=false
-      Word Completion=true
-      Word Completion Minimal Word Length=3
-      Word Completion Remove Tail=true
+      "KTextEditor View" = {
+        "Allow Mark Menu".value = true;
+        "Auto Brackets".value = true;
+        "Auto Center Lines".value = 0;
+        "Auto Completion".value = true;
+        "Auto Completion Preselect First Entry".value = true;
+        "Backspace Remove Composed Characters".value = false;
+        "Bookmark Menu Sorting".value = 0;
+        "Bracket Match Preview".value = false;
+        "Cycle Through Bookmarks".value = true;
+        "Default Mark Type".value = 1;
+        "Disable bracket match highlight if inactive".value = false;
+        "Disable current line highlight if inactive".value = false;
+        "Dynamic Word Wrap".value = true;
+        "Dynamic Word Wrap Align Indent".value = 80;
+        "Dynamic Word Wrap At Static Marker".value = false;
+        "Dynamic Word Wrap Indicators".value = 1;
+        "Dynamic Wrap not at word boundaries".value = false;
+        "Enable Accessibility".value = true;
+        "Enable Tab completion".value = false;
+        "Enter To Insert Completion".value = true;
+        "Fold First Line".value = false;
+        "Folding Bar".value = true;
+        "Folding Preview".value = true;
+        "Hide cursor if inactive".value = false;
+        "Icon Bar".value = false;
+        "Input Mode".value = 0;
+        "Keyword Completion".value = true;
+        "Line Modification".value = true;
+        "Line Numbers".value = true;
+        "Max Clipboard History Entries".value = 20;
+        "Maximum Search History Size".value = 100;
+        "Mouse Paste At Cursor Position".value = false;
+        "Multiple Cursor Modifier".value = 134217728;
+        "Persistent Selection".value = false;
+        "Scroll Bar Marks".value = false;
+        "Scroll Bar Mini Map All".value = true;
+        "Scroll Bar Mini Map Width".value = 60;
+        "Scroll Bar MiniMap".value = true;
+        "Scroll Bar Preview".value = true;
+        "Scroll Past End".value = false;
+        "Search/Replace Flags".value = 140;
+        "Shoe Line Ending Type in Statusbar".value = false;
+        "Show Documentation With Completion".value = true;
+        "Show File Encoding".value = true;
+        "Show Folding Icons On Hover Only".value = true;
+        "Show Line Count".value = true;
+        "Show Scrollbars".value = 0;
+        "Show Statusbar Dictionary".value = true;
+        "Show Statusbar Highlighting Mode".value = true;
+        "Show Statusbar Input Mode".value = true;
+        "Show Statusbar Line Column".value = true;
+        "Show Statusbar Tab Settings".value = true;
+        "Show Word Count".value = true;
+        "Smart Copy Cut".value = true;
+        "Statusbar Line Column Compact Mode".value = true;
+        "Text Drag And Drop".value = true;
+        "User Sets Of Chars To Enclose Selection".value = "";
+        "Vi Input Mode Steal Keys".value = false;
+        "Vi Relative Line Numbers".value = false;
+        "Word Completion".value = true;
+        "Word Completion Minimal Word Length".value = 3;
+        "Word Completion Remove Tail".value = true;
+      };
 
-      [MostFrequentlyUsedSymbols]
-      counts=
-      paths=
-
-      [NewFileWizard]
-      UseWizardWhenCreatingEmptyFile=true
-    ''; */
+      "NewFileWizard" = {
+        "UseWizardWhenCreatingEmptyFile".value = true;
+      };
+    };
   };
 }
