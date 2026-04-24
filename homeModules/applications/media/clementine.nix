be1c1537c684a903d417d04172306dc508cab8be
xerhaxs <github.wjtpq@slmail.me>
2026-04-24 12:14:18 +0200

    add clementine config

--- homeModules/applications/media/clementine.nix
+++ homeModules/applications/media/clementine.nix
@@ -22,207 +22,118 @@
       clementine
     ];
 
-    /* xdg.configFile."Clementine/Clementine.conf".text = ''
-      [General]
-      hidden=true
+    home.activation.clementineConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      CONFIG="$HOME/.config/Clementine/Clementine.conf"
+      if [ -f "$CONFIG" ]; then
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" General    hidden                        true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Analyzer   framerate                     25
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Analyzer   type                          BlockAnalyzer
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Appearance b_hide_filter_toolbar         false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Appearance b_use_sys_icons               true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Appearance use-custom-set                false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Equalizer  enabled                       false
 
-      [Analyzer]
-      framerate=25
-      type=BlockAnalyzer
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch enabled_classicalradio      false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch enabled_di                  false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch enabled_icecast             false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch enabled_intergalacticfm     false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch enabled_jamendo             false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch enabled_jazzradio           false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch enabled_library             true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch enabled_magnatune           false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch enabled_radiobrowser        false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch enabled_radiotunes          false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch enabled_rockradio           false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch enabled_savedradio          false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch enabled_somafm              false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch enabled_subsonic            false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch group_by1                   1
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch group_by2                   2
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch group_by3                   0
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch show_providers              true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GlobalSearch show_suggestions            true
 
-      [Appearance]
-      b_hide_filter_toolbar=false
-      b_use_sys_icons=true
-      use-custom-set=false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GstEngine   bufferduration              4000
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GstEngine   bufferminfill               33
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GstEngine   monoplayback                false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GstEngine   rgcompression               true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GstEngine   rgenabled                   false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GstEngine   rgmode                      0
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GstEngine   samplerate                  -1
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" GstEngine   sink                        autoaudiosink
 
-      [Equalizer]
-      enabled=false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" LibraryBackend save_ratings_in_file     true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" LibraryBackend save_statistics_in_file  true
 
-      [GlobalSearch]
-      enabled_classicalradio=false
-      enabled_di=false
-      enabled_icecast=false
-      enabled_intergalacticfm=false
-      enabled_jamendo=false
-      enabled_jazzradio=false
-      enabled_library=true
-      enabled_magnatune=false
-      enabled_radiobrowser=false
-      enabled_radiotunes=false
-      enabled_rockradio=false
-      enabled_savedradio=false
-      enabled_somafm=false
-      enabled_subsonic=false
-      group_by1=1
-      group_by2=2
-      group_by3=0
-      provider_order=library, Box, Seafile, classicalradio, di, dropbox, google_drive, icecast, intergalacticfm, jamendo, jazzradio, magnatune, radiobrowser, radiotunes, rockradio, savedradio, skydrive, somafm, subsonic
-      show_providers=true
-      show_suggestions=true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" LibraryView auto_open                   true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" LibraryView pretty_covers               true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" LibraryView show_dividers               true
 
-      [GstEngine]
-      bufferduration=4000
-      bufferminfill=33
-      device=
-      format=
-      monoplayback=false
-      rgcompression=true
-      rgenabled=false
-      rgmode=0
-      samplerate=-1
-      sink=autoaudiosink
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" LibraryWatcher cover_art_patterns       "front, cover"
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" LibraryWatcher monitor                  true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" LibraryWatcher startup_scan             true
 
-      [LibraryBackend]
-      save_ratings_in_file=true
-      save_statistics_in_file=true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow current_tab                  1
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow doubleclick_addmode          1
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow doubleclick_playlist_addmode 2
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow doubleclick_playmode         2
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow group_by1                    7
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow group_by2                    2
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow group_by3                    0
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow hidden                       false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow keeprunning                  false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow maximized                    true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow menu_playmode                2
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow resume_playback_after_start  true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow scrolltrayicon               true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow show_sidebar                 true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow showtray                     false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow startupbehaviour             1
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" MainWindow tab_mode                     1
 
-      [LibraryConfig]
-      last_path=
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Moodbar    calculate                    true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Moodbar    save_alongside_originals     false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Moodbar    show                         true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Moodbar    style                        4
 
-      [LibraryView]
-      auto_open=true
-      pretty_covers=true
-      show_dividers=true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" NetworkRemote allow_downloads           false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" NetworkRemote only_non_public_ip        true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" NetworkRemote port                      5500
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" NetworkRemote use_auth_code             false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" NetworkRemote use_remote                false
 
-      [LibraryWatcher]
-      cover_art_patterns=front, cover
-      monitor=true
-      skip_file_extensions=@Invalid()
-      startup_scan=true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" OSD         Behaviour                   0
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" OSD         CustomTextEnabled           false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" OSD         ShowArt                     true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" OSD         ShowOnPausePlayback         true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" OSD         ShowOnPlayModeChange        true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" OSD         ShowOnVolumeChange          false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" OSD         Timeout                     5000
 
-      [MainWindow]
-      current_tab=1
-      doubleclick_addmode=1
-      doubleclick_playlist_addmode=2
-      doubleclick_playmode=2
-      group_by1=7
-      group_by2=2
-      group_by3=0
-      hidden=false
-      keeprunning=false
-      maximized=true
-      menu_playmode=2
-      playback_position=125
-      playback_state=3
-      resume_playback_after_start=true
-      scrolltrayicon=true
-      show_sidebar=true
-      showtray=false
-      startupbehaviour=1
-      tab_index_0=0
-      tab_index_1=1
-      tab_index_2=2
-      tab_index_3=3
-      tab_index_4=4
-      tab_index_5=5
-      tab_index_6=6
-      tab_index_7=7
-      tab_index_8=8
-      tab_mode=1
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Player      AutoCrossfadeEnabled        false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Player      CrossfadeEnabled            true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Player      FadeoutDuration             2000
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Player      FadeoutEnabled              true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Player      FadeoutPauseDuration        250
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Player      FadeoutPauseEnabled         false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Player      NoCrossfadeSameAlbum        true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Player      menu_previousmode           2
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Player      play_count_short_duration   false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Player      seek_step_sec               10
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Player      stop_play_if_fail           true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" Player      volume                      100
 
-      [Moodbar]
-      calculate=true
-      save_alongside_originals=false
-      show=true
-      style=4
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" PlaylistTabBar warn_close_playlist      true
 
-      [NetworkRemote]
-      allow_downloads=false
-      auth_code=62493
-      convert_lossless=false
-      files_music_extensions=aac, alac, flac, m3u, m4a, mp3, ogg, wav, wmv
-      files_root_folder=
-      last_output_format=audio/x-vorbis
-      only_non_public_ip=true
-      port=5500
-      use_auth_code=false
-      use_remote=false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" SongMetadata guess_metadata             true
 
-      [OSD]
-      Behaviour=0
-      CustomText1=
-      CustomText2=
-      CustomTextEnabled=false
-      ShowArt=true
-      ShowOnPausePlayback=true
-      ShowOnPlayModeChange=true
-      ShowOnVolumeChange=false
-      Timeout=5000
-
-      [Player]
-      AutoCrossfadeEnabled=false
-      CrossfadeEnabled=true
-      FadeoutDuration=2000
-      FadeoutEnabled=true
-      FadeoutPauseDuration=250
-      FadeoutPauseEnabled=false
-      NoCrossfadeSameAlbum=true
-      max_numprocs_tagclients=24
-      menu_previousmode=2
-      play_count_short_duration=false
-      seek_step_sec=10
-      stop_play_if_fail=true
-      volume=100
-
-      PlaylistTabBar]
-      warn_close_playlist=true
-
-      [SongInfo]
-      font_size=8.5
-      search_order=lyrics.wikia.com, lyricstime.com, lyricsreg.com, lyricsmania.com, metrolyrics.com, azlyrics.com, songlyrics.com, elyrics.net, lyricsdownload.com, lyrics.com, lyricsbay.com, directlyrics.com, loudson.gs, teksty.org, darklyrics.com
-
-      [SongMetadata]
-      guess_metadata=true
-
-      [Subsonic]
-      group_by1=7
-      group_by2=2
-      group_by3=0
-
-      [Transcoder]
-      fdkaacenc\bitrate=0
-      flacenc\quality=5
-      flacenc\streamable-subset=true
-      lamemp3enc\bitrate=128
-      lamemp3enc\cbr=true
-      lamemp3enc\encoding-engine-quality=1
-      lamemp3enc\mono=false
-      lamemp3enc\quality=4
-      lamemp3enc\target=1
-      opusenc\bitrate=128000
-      speexenc\abr=0
-      speexenc\bitrate=0
-      speexenc\complexity=3
-      speexenc\dtx=false
-      speexenc\mode=0
-      speexenc\nframes=1
-      speexenc\quality=8
-      speexenc\vad=false
-      speexenc\vbr=false
-      vorbisenc\bitrate=-1
-      vorbisenc\managed=false
-      vorbisenc\max-bitrate=-1
-      vorbisenc\min-bitrate=-1
-      vorbisenc\quality=0.3
-
-      [WiimotedevActions]
-      1=9
-      128=1
-      16=6
-      16384=2
-      2=13
-      256=2
-      32=5
-      4=0
-      8192=1
-
-      [WiimotedevSettings]
-      device=1
-      enabled=false
-      first_conf=false
-      only_when_focused=false
-      use_active_action=true
-      use_notification=true
-    ''; */
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" WiimotedevSettings device              1
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" WiimotedevSettings enabled             false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" WiimotedevSettings first_conf          false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" WiimotedevSettings only_when_focused   false
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" WiimotedevSettings use_active_action   true
+        ${pkgs.crudini}/bin/crudini --set "$CONFIG" WiimotedevSettings use_notification    true
+      fi
+    '';
   };
 }
