8680e0623e60432fd4b9f842fd4f5136a761d388
xerhaxs <github.wjtpq@slmail.me>
2026-04-24 12:14:11 +0200

    add heroic config

--- homeModules/applications/gaming/heroic.nix
+++ homeModules/applications/gaming/heroic.nix
@@ -22,88 +22,69 @@
       heroic
     ];
 
-    /* xdg.configFile."heroic/config.json".text = ''
-      {
-        "defaultSettings": {
-          "analyticsOptIn": false,
-          "checkUpdatesInterval": 10,
-          "enableUpdates": false,
-          "addDesktopShortcuts": false,
-          "addStartMenuShortcuts": false,
-          "autoInstallDxvk": true,
-          "autoInstallVkd3d": true,
-          "autoInstallDxvkNvapi": true,
-          "addSteamShortcuts": false,
-          "preferSystemLibs": false,
-          "checkForUpdatesOnStartup": false,
-          "autoUpdateGames": false,
-          "customWinePaths": [],
-          "defaultInstallPath": "${config.xdg.userDirs.extraConfig.GAMES}/Heroic",
-          "libraryTopSection": "recently_played",
-          "defaultSteamPath": "${config.xdg.userDirs.extraConfig.GAMES}/Steam",
-          "defaultWinePrefix": "${config.xdg.userDirs.extraConfig.GAMES}/Heroic/Wine",
-          "hideChangelogsOnStartup": false,
-          "language": "de",
-          "maxWorkers": 0,
-          "minimizeOnLaunch": false,
-          "nvidiaPrime": false,
-          "enviromentOptions": [],
-          "wrapperOptions": [],
-          "showFps": false,
-          "useGameMode": true,
-          "wineCrossoverBottle": "Heroic",
-          "winePrefix": "${config.xdg.userDirs.extraConfig.GAMES}/Heroic/Prefixes/default",
-          "wineVersion": {
-            "bin": "${config.home.homeDirectory}.config/heroic/tools/wine/Wine-GE-latest/bin/wine",
-            "name": "Wine-GE-latest",
-            "type": "wine",
-            "lib": "${config.home.homeDirectory}/.config/heroic/tools/wine/Wine-GE-latest/lib64",
-            "lib32": "${config.home.homeDirectory}/.config/heroic/tools/wine/Wine-GE-latest/lib",
-            "wineserver": "${config.home.homeDirectory}/.config/heroic/tools/wine/Wine-GE-latest/bin/wineserver"
-          },
-          "enableEsync": true,
-          "enableFsync": true,
-          "enableMsync": false,
-          "enableWineWayland": false,
-          "enableHDR": false,
-          "enableWoW64": false,
-          "eacRuntime": false,
-          "battlEyeRuntime": false,
-          "framelessWindow": false,
-          "beforeLaunchScriptPath": "",
-          "afterLaunchScriptPath": "",
-          "disableUMU": false,
-          "verboseLogs": false,
-          "downloadProtonToSteam": false,
-          "advertiseAvxForRosetta": false,
-          "noTrayIcon": false,
-          "showValveProton": true,
-          "gamescope": {
-            "enableUpscaling": false,
-            "enableLimiter": false,
-            "windowType": "fullscreen",
-            "gameWidth": "",
-            "gameHeight": "",
-            "upscaleHeight": "",
-            "upscaleWidth": "",
-            "upscaleMethod": "fsr",
-            "fpsLimiter": "",
-            "fpsLimiterNoFocus": "",
-            "additionalOptions": ""
-          },
-          "showMangohud": false,
-          "experimentalFeatures": {
-            "enableNewDesign": false,
-            "enableHelp": false,
-            "automaticWinetricksFixes": true
-          },
-          "customThemesPath": "${config.home.homeDirectory}/.config/heroic/themes",
-          "downloadNoHttps": true,
-          "exitToTray": false,
-          "allowInstallationBrokenAnticheat": true
-        },
-        "version": "v0"
-      }
-    ''; */
+    home.activation.heroicConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      CONFIG="${config.xdg.configHome}/heroic/config.json"
+      if [ -f "$CONFIG" ]; then
+        ${pkgs.jq}/bin/jq '
+          .defaultSettings.analyticsOptIn                = false |
+          .defaultSettings.checkUpdatesInterval          = 10    |
+          .defaultSettings.enableUpdates                 = false |
+          .defaultSettings.addDesktopShortcuts           = false |
+          .defaultSettings.addStartMenuShortcuts         = false |
+          .defaultSettings.autoInstallDxvk               = true  |
+          .defaultSettings.autoInstallVkd3d              = true  |
+          .defaultSettings.autoInstallDxvkNvapi          = true  |
+          .defaultSettings.addSteamShortcuts             = false |
+          .defaultSettings.preferSystemLibs              = false |
+          .defaultSettings.checkForUpdatesOnStartup      = false |
+          .defaultSettings.autoUpdateGames               = false |
+          .defaultSettings.defaultInstallPath            = "${config.xdg.userDirs.extraConfig.GAMES}/Heroic" |
+          .defaultSettings.libraryTopSection             = "recently_played" |
+          .defaultSettings.defaultSteamPath              = "${config.xdg.userDirs.extraConfig.GAMES}/Steam" |
+          .defaultSettings.defaultWinePrefix             = "${config.xdg.userDirs.extraConfig.GAMES}/Heroic/Wine" |
+          .defaultSettings.hideChangelogsOnStartup       = false |
+          .defaultSettings.language                      = "de"  |
+          .defaultSettings.maxWorkers                    = 0     |
+          .defaultSettings.minimizeOnLaunch              = false |
+          .defaultSettings.nvidiaPrime                   = false |
+          .defaultSettings.showFps                       = false |
+          .defaultSettings.useGameMode                   = true  |
+          .defaultSettings.wineCrossoverBottle           = "Heroic" |
+          .defaultSettings.winePrefix                    = "${config.xdg.userDirs.extraConfig.GAMES}/Heroic/Prefixes/default" |
+          .defaultSettings.wineVersion.bin               = "${config.home.homeDirectory}/.config/heroic/tools/wine/Wine-GE-latest/bin/wine" |
+          .defaultSettings.wineVersion.name              = "Wine-GE-latest" |
+          .defaultSettings.wineVersion.type              = "wine" |
+          .defaultSettings.wineVersion.lib               = "${config.home.homeDirectory}/.config/heroic/tools/wine/Wine-GE-latest/lib64" |
+          .defaultSettings.wineVersion.lib32             = "${config.home.homeDirectory}/.config/heroic/tools/wine/Wine-GE-latest/lib" |
+          .defaultSettings.wineVersion.wineserver        = "${config.home.homeDirectory}/.config/heroic/tools/wine/Wine-GE-latest/bin/wineserver" |
+          .defaultSettings.enableEsync                   = true  |
+          .defaultSettings.enableFsync                   = true  |
+          .defaultSettings.enableMsync                   = false |
+          .defaultSettings.enableWineWayland             = false |
+          .defaultSettings.enableHDR                     = false |
+          .defaultSettings.enableWoW64                   = false |
+          .defaultSettings.eacRuntime                    = false |
+          .defaultSettings.battlEyeRuntime               = false |
+          .defaultSettings.framelessWindow               = false |
+          .defaultSettings.disableUMU                    = false |
+          .defaultSettings.verboseLogs                   = false |
+          .defaultSettings.downloadProtonToSteam         = false |
+          .defaultSettings.noTrayIcon                    = false |
+          .defaultSettings.showValveProton               = true  |
+          .defaultSettings.gamescope.enableUpscaling     = false |
+          .defaultSettings.gamescope.enableLimiter       = false |
+          .defaultSettings.gamescope.windowType          = "fullscreen" |
+          .defaultSettings.gamescope.upscaleMethod       = "fsr" |
+          .defaultSettings.showMangohud                  = false |
+          .defaultSettings.experimentalFeatures.automaticWinetricksFixes = true  |
+          .defaultSettings.experimentalFeatures.enableNewDesign          = false |
+          .defaultSettings.experimentalFeatures.enableHelp               = false |
+          .defaultSettings.customThemesPath              = "${config.home.homeDirectory}/.config/heroic/themes" |
+          .defaultSettings.downloadNoHttps               = true  |
+          .defaultSettings.exitToTray                    = false |
+          .defaultSettings.allowInstallationBrokenAnticheat = true
+        ' "$CONFIG" > "$CONFIG.tmp" && mv "$CONFIG.tmp" "$CONFIG"
+      fi
+    '';
   };
 }
