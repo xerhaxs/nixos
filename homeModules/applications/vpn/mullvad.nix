7e4dd9ece43ad46e70350d6899aa499aa392b706
xerhaxs <github.wjtpq@slmail.me>
2026-05-03 20:41:41 +0200

    fix mullvad daemon

--- homeModules/applications/vpn/mullvad.nix
+++ homeModules/applications/vpn/mullvad.nix
@@ -37,19 +37,25 @@
     };
 
     home.activation.mullvadSetup = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      TIMEOUT=30
+      COUNT=0
       until ${pkgs.mullvad}/bin/mullvad status > /dev/null 2>&1; do
+        if [ $COUNT -ge $TIMEOUT ]; then
+          echo "Mullvad daemon not available, skipping setup"
+          exit 0
+        fi
+        COUNT=$((COUNT + 1))
         sleep 1
       done
 
       if ! ${pkgs.mullvad}/bin/mullvad account get 2>&1 | grep -q "Mullvad account:"; then
         ${pkgs.mullvad}/bin/mullvad account login $(tr -d '[:space:]' < ${
-          osConfig.sops.secrets."mullvad".path
-        })
+          config.sops.secrets."mullvad".path
+        }) || true
       fi
 
-      ${pkgs.mullvad}/bin/mullvad lan set allow
+      ${pkgs.mullvad}/bin/mullvad lan set allow || true
     '';
-
     home.persistence."/persistent" = lib.mkIf osConfig.nixos.disko.disko-luks-btrfs-tmpfs.enable {
       directories = [
         ".config/Mullvad VPN"
