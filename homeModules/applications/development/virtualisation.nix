d9203e25c05e24b21c10777ab7e8b22dc79ca63c
xerhaxs <github.wjtpq@slmail.me>
2026-05-03 15:27:48 +0200

    add virt manager settings

--- homeModules/applications/development/virtualisation.nix
+++ homeModules/applications/development/virtualisation.nix
@@ -22,5 +22,50 @@
       spice-gtk
       virt-viewer
     ];
+
+    dconf.settings = {
+      "org/virt-manager/virt-manager" = {
+        xmleditor-enabled = true;
+      };
+
+      "org/virt-manager/virt-manager/confirm" = {
+        delete-storage = true;
+        forcepoweroff = false;
+        poweroff = false;
+        removedev = true;
+        unapplied-dev = true;
+      };
+
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = [ "qemu:///system" ];
+        uris = [ "qemu:///system" ];
+      };
+
+      "org/virt-manager/virt-manager/console" = {
+        resize-guest = 1;
+        scaling = 2;
+      };
+
+      "org/virt-manager/virt-manager/details" = {
+        show-toolbar = true;
+      };
+
+      "org/virt-manager/virt-manager/new-vm" = {
+        graphics-type = "system";
+      };
+
+      "org/virt-manager/virt-manager/stats" = {
+        enable-disk-poll = true;
+        enable-memory-poll = true;
+        enable-net-poll = true;
+      };
+
+      "org/virt-manager/virt-manager/vmlist-fields" = {
+        disk-usage = true;
+        host-cpu-usage = true;
+        memory-usage = true;
+        network-traffic = true;
+      };
+    };
   };
 }
