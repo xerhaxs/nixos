3f424cd4928cc7e77c1855c7c7c6279611c03c23
xerhaxs <github.wjtpq@slmail.me>
2026-05-04 18:28:19 +0200

    update heroic config.json to apply theme

--- homeModules/applications/gaming/heroic.nix
+++ homeModules/applications/gaming/heroic.nix
@@ -35,70 +35,78 @@
 
     home.activation.heroicConfig = lib.hm.dag.entryAfter [ "writeBoundary" "heroicCatppuccinTheme" ] ''
       CONFIG="${config.xdg.configHome}/heroic/config.json"
+      STORE="${config.xdg.configHome}/heroic/store/config.json"
+
+      JQ_FILTER='
+        .defaultSettings.analyticsOptIn                = false |
+        .defaultSettings.checkUpdatesInterval          = 10    |
+        .defaultSettings.enableUpdates                 = false |
+        .defaultSettings.addDesktopShortcuts           = false |
+        .defaultSettings.addStartMenuShortcuts         = false |
+        .defaultSettings.autoInstallDxvk               = true  |
+        .defaultSettings.autoInstallVkd3d              = true  |
+        .defaultSettings.autoInstallDxvkNvapi          = true  |
+        .defaultSettings.addSteamShortcuts             = false |
+        .defaultSettings.preferSystemLibs              = false |
+        .defaultSettings.checkForUpdatesOnStartup      = false |
+        .defaultSettings.autoUpdateGames               = false |
+        .defaultSettings.defaultInstallPath            = "${config.xdg.userDirs.extraConfig.GAMES}/Heroic" |
+        .defaultSettings.libraryTopSection             = "recently_played" |
+        .defaultSettings.defaultSteamPath              = "${config.xdg.userDirs.extraConfig.GAMES}/Steam" |
+        .defaultSettings.defaultWinePrefix             = "${config.xdg.userDirs.extraConfig.GAMES}/Heroic/Wine" |
+        .defaultSettings.hideChangelogsOnStartup       = false |
+        .defaultSettings.language                      = "de"  |
+        .defaultSettings.maxWorkers                    = 0     |
+        .defaultSettings.minimizeOnLaunch              = false |
+        .defaultSettings.nvidiaPrime                   = false |
+        .defaultSettings.showFps                       = false |
+        .defaultSettings.useGameMode                   = true  |
+        .defaultSettings.wineCrossoverBottle           = "Heroic" |
+        .defaultSettings.winePrefix                    = "${config.xdg.userDirs.extraConfig.GAMES}/Heroic/Prefixes/default" |
+        .defaultSettings.wineVersion.bin               = "${config.home.homeDirectory}/.config/heroic/tools/wine/Wine-GE-latest/bin/wine" |
+        .defaultSettings.wineVersion.name              = "Wine-GE-latest" |
+        .defaultSettings.wineVersion.type              = "wine" |
+        .defaultSettings.wineVersion.lib               = "${config.home.homeDirectory}/.config/heroic/tools/wine/Wine-GE-latest/lib64" |
+        .defaultSettings.wineVersion.lib32             = "${config.home.homeDirectory}/.config/heroic/tools/wine/Wine-GE-latest/lib" |
+        .defaultSettings.wineVersion.wineserver        = "${config.home.homeDirectory}/.config/heroic/tools/wine/Wine-GE-latest/bin/wineserver" |
+        .defaultSettings.enableEsync                   = true  |
+        .defaultSettings.enableFsync                   = true  |
+        .defaultSettings.enableMsync                   = false |
+        .defaultSettings.enableWineWayland             = false |
+        .defaultSettings.enableHDR                     = false |
+        .defaultSettings.enableWoW64                   = false |
+        .defaultSettings.eacRuntime                    = false |
+        .defaultSettings.battlEyeRuntime               = false |
+        .defaultSettings.framelessWindow               = false |
+        .defaultSettings.disableUMU                    = false |
+        .defaultSettings.verboseLogs                   = false |
+        .defaultSettings.downloadProtonToSteam         = false |
+        .defaultSettings.noTrayIcon                    = false |
+        .defaultSettings.showValveProton               = true  |
+        .defaultSettings.gamescope.enableUpscaling     = false |
+        .defaultSettings.gamescope.enableLimiter       = false |
+        .defaultSettings.gamescope.windowType          = "fullscreen" |
+        .defaultSettings.gamescope.upscaleMethod       = "fsr" |
+        .defaultSettings.showMangohud                  = false |
+        .defaultSettings.experimentalFeatures.automaticWinetricksFixes = true  |
+        .defaultSettings.experimentalFeatures.enableNewDesign          = false |
+        .defaultSettings.experimentalFeatures.enableHelp               = false |
+        .defaultSettings.customThemesPath              = "${config.home.homeDirectory}/.config/heroic/themes" |
+        .defaultSettings.theme                         = "catppuccin-${lib.strings.toLower osConfig.nixos.theme.catppuccin.flavor}-${lib.strings.toLower osConfig.nixos.theme.catppuccin.accent}.css" |
+        .defaultSettings.downloadNoHttps               = true  |
+        .defaultSettings.exitToTray                    = false |
+        .defaultSettings.allowInstallationBrokenAnticheat = true
+      '
+
       if [ -f "$CONFIG" ]; then
-        ${pkgs.jq}/bin/jq '
-          .defaultSettings.analyticsOptIn                = false |
-          .defaultSettings.checkUpdatesInterval          = 10    |
-          .defaultSettings.enableUpdates                 = false |
-          .defaultSettings.addDesktopShortcuts           = false |
-          .defaultSettings.addStartMenuShortcuts         = false |
-          .defaultSettings.autoInstallDxvk               = true  |
-          .defaultSettings.autoInstallVkd3d              = true  |
-          .defaultSettings.autoInstallDxvkNvapi          = true  |
-          .defaultSettings.addSteamShortcuts             = false |
-          .defaultSettings.preferSystemLibs              = false |
-          .defaultSettings.checkForUpdatesOnStartup      = false |
-          .defaultSettings.autoUpdateGames               = false |
-          .defaultSettings.defaultInstallPath            = "${config.xdg.userDirs.extraConfig.GAMES}/Heroic" |
-          .defaultSettings.libraryTopSection             = "recently_played" |
-          .defaultSettings.defaultSteamPath              = "${config.xdg.userDirs.extraConfig.GAMES}/Steam" |
-          .defaultSettings.defaultWinePrefix             = "${config.xdg.userDirs.extraConfig.GAMES}/Heroic/Wine" |
-          .defaultSettings.hideChangelogsOnStartup       = false |
-          .defaultSettings.language                      = "de"  |
-          .defaultSettings.maxWorkers                    = 0     |
-          .defaultSettings.minimizeOnLaunch              = false |
-          .defaultSettings.nvidiaPrime                   = false |
-          .defaultSettings.showFps                       = false |
-          .defaultSettings.useGameMode                   = true  |
-          .defaultSettings.wineCrossoverBottle           = "Heroic" |
-          .defaultSettings.winePrefix                    = "${config.xdg.userDirs.extraConfig.GAMES}/Heroic/Prefixes/default" |
-          .defaultSettings.wineVersion.bin               = "${config.home.homeDirectory}/.config/heroic/tools/wine/Wine-GE-latest/bin/wine" |
-          .defaultSettings.wineVersion.name              = "Wine-GE-latest" |
-          .defaultSettings.wineVersion.type              = "wine" |
-          .defaultSettings.wineVersion.lib               = "${config.home.homeDirectory}/.config/heroic/tools/wine/Wine-GE-latest/lib64" |
-          .defaultSettings.wineVersion.lib32             = "${config.home.homeDirectory}/.config/heroic/tools/wine/Wine-GE-latest/lib" |
-          .defaultSettings.wineVersion.wineserver        = "${config.home.homeDirectory}/.config/heroic/tools/wine/Wine-GE-latest/bin/wineserver" |
-          .defaultSettings.enableEsync                   = true  |
-          .defaultSettings.enableFsync                   = true  |
-          .defaultSettings.enableMsync                   = false |
-          .defaultSettings.enableWineWayland             = false |
-          .defaultSettings.enableHDR                     = false |
-          .defaultSettings.enableWoW64                   = false |
-          .defaultSettings.eacRuntime                    = false |
-          .defaultSettings.battlEyeRuntime               = false |
-          .defaultSettings.framelessWindow               = false |
-          .defaultSettings.disableUMU                    = false |
-          .defaultSettings.verboseLogs                   = false |
-          .defaultSettings.downloadProtonToSteam         = false |
-          .defaultSettings.noTrayIcon                    = false |
-          .defaultSettings.showValveProton               = true  |
-          .defaultSettings.gamescope.enableUpscaling     = false |
-          .defaultSettings.gamescope.enableLimiter       = false |
-          .defaultSettings.gamescope.windowType          = "fullscreen" |
-          .defaultSettings.gamescope.upscaleMethod       = "fsr" |
-          .defaultSettings.showMangohud                  = false |
-          .defaultSettings.experimentalFeatures.automaticWinetricksFixes = true  |
-          .defaultSettings.experimentalFeatures.enableNewDesign          = false |
-          .defaultSettings.experimentalFeatures.enableHelp               = false |
-          .defaultSettings.customThemesPath              = "${config.home.homeDirectory}/.config/heroic/themes" |
-          .defaultSettings.theme = "catppuccin-${lib.strings.toLower osConfig.nixos.theme.catppuccin.flavor}-${lib.strings.toLower osConfig.nixos.theme.catppuccin.accent}" |
-          .defaultSettings.downloadNoHttps               = true  |
-          .defaultSettings.exitToTray                    = false |
-          .defaultSettings.allowInstallationBrokenAnticheat = true
-        ' "$CONFIG" > "$CONFIG.tmp" && mv "$CONFIG.tmp" "$CONFIG"
+        ${pkgs.jq}/bin/jq "$JQ_FILTER" "$CONFIG" > "$CONFIG.tmp" && mv "$CONFIG.tmp" "$CONFIG"
+      fi
+
+      if [ -f "$STORE" ]; then
+        ${pkgs.jq}/bin/jq "$JQ_FILTER" "$STORE" > "$STORE.tmp" && mv "$STORE.tmp" "$STORE"
       fi
     '';
-
+    
     home.persistence."/persistent" = lib.mkIf osConfig.nixos.disko.disko-luks-btrfs-tmpfs.enable {
       directories = [
         ".config/heroic"
