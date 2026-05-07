01a41a24ea46315f5e87f8228f1204f8b7ac4596
xerhaxs <github.wjtpq@slmail.me>
2026-05-07 16:41:24 +0200

    setup karere whatsapp client + element config

--- homeModules/applications/communication/social.nix
+++ homeModules/applications/communication/social.nix
@@ -25,8 +25,38 @@
       mumble
       signal-desktop
       telegram-desktop
-      karere
+      (karere.overrideAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ makeWrapper ];
+        postInstall = (old.postInstall or "") + ''
+                  wrapProgram $out/bin/karere \
+          --unset WAYLAND_DISPLAY \
+          --prefix GST_PLUGIN_PATH : "${
+            lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
+              gst_all_1.gstreamer
+              gst_all_1.gst-plugins-base
+              gst_all_1.gst-plugins-good
+            ]
+          }"
+        '';
+      }))
     ];
+
+    home.activation.elementElectronConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      CONFIG="$HOME/.config/Element/electron-config.json"
+      if [ -f "$CONFIG" ]; then
+        ${pkgs.jq}/bin/jq '
+          .warnBeforeExit              = false |
+          .minimizeToTray              = false |
+          .spellCheckerEnabled         = true  |
+          .autoHideMenuBar             = false |
+          .disableHardwareAcceleration = false |
+          .enableContentProtection     = false |
+          .openAtLoginMinimised        = false |
+          .safeStorageBackend          = "kwallet6" |
+          .locale                      = ["en"]
+        ' "$CONFIG" > "$CONFIG.tmp" && mv "$CONFIG.tmp" "$CONFIG"
+      fi
+    '';
 
     home.persistence."/persistent" = lib.mkIf osConfig.nixos.disko.disko-luks-btrfs-tmpfs.enable {
       directories = [
