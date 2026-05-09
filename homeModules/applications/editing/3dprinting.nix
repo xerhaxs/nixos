80689e660856d6c0ffb6ff4fd087225c1bd61f36
xerhaxs <github.wjtpq@slmail.me>
2026-05-09 18:00:29 +0200

    setup 3dprinting / orca

--- homeModules/applications/editing/3dprinting.nix
+++ homeModules/applications/editing/3dprinting.nix
@@ -19,9 +19,17 @@
 
   config = lib.mkIf config.homeManager.applications.editing."3dprinting".enable {
     home.packages = with pkgs; [
-      #blender
       orca-slicer
       freecad-wayland
     ];
+
+    home.persistence."/persistent" = lib.mkIf osConfig.nixos.disko.disko-luks-btrfs-tmpfs.enable {
+      directories = [
+        ".config/FreeCAD"
+        ".config/OrcaSlicer"
+        ".local/share/FreeCAD"
+        ".local/share/orca-slicer"
+      ];
+    };
   };
 }
