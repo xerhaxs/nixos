87ae1ea5632a254cfe48b5adfdf863982f31d20b
xerhaxs <github.wjtpq@slmail.me>
2026-05-16 13:26:04 +0200

    fix mic mute unmute

--- hosts/NixOS-Desktop/home/home.nix
+++ hosts/NixOS-Desktop/home/home.nix
@@ -13,7 +13,14 @@
 
   programs.plasma = {
     workspace = {
-      wallpaper = lib.mkForce "${config.xdg.userDirs.pictures}/Desktopbilder/JWST/compress/52338778943_9704c200b4_o.jpg";
+      wallpaper = "${config.xdg.userDirs.pictures}/Desktopbilder/JWST/compress/52338778943_9704c200b4_o.jpg";
+    };
+
+    configFile = {
+      "plasmaparc"."General" = {
+        "GlobalMuteSourcesMutedDevices" =
+          "alsa_input.usb-Burr-Brown_from_TI_USB_Audio_CODEC-00.analog-stereo-input.0";
+      };
     };
   };
 
